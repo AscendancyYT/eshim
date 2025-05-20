91c87d252ff013706a62658e9685df3b31379edc
Azizbek <thedroxmc@gmail.com>
2025-05-20 18:22:38 +0500

    login is now responsive

---
+++ functions/purchase.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+  const title = document.getElementById("purchaseTitle");
+  title.style.top = "20px"; // moves it up
+  title.style.transform = "translateX(-50%)";
+});

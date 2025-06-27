c9731000e5b7db9daca1ad6797621c700029cff9
Azizbek <thedroxmc@gmail.com>
2025-06-27 14:43:26 +0500

    Fetching Accaunt ID functionality

--- functions/buy.js
+++ functions/buy.js
@@ -1,9 +1,26 @@
-// let accIDInput = document.querySelector(".accIDInput");
+let accIDInput = document.querySelector(".accIDInput");
+let buyBtn = document.querySelector(".buyBtn")
+let userTelegram = localStorage.getItem("telegram");
 
-// async function getUsers() {
-//   let userList = await axios.get("https://67c8964c0acf98d07087272b.mockapi.io/users");
+async function getUsers() {
+  await axios
+    .get("https://67c8964c0acf98d07087272b.mockapi.io/users")
+    .then((response) => {
+      return response;
+    });
+}
 
-//   return(userList)
-// };
+async function fillID() {
+  await axios
+    .get(`https://67c8964c0acf98d07087272b.mockapi.io/users?${userTelegram}`)
+    .then((response) => {
+      accIDInput.value = response.data[0].accID
+      console.log(response);
+    });
+}
 
-// getUsers()
+buyBtn.onclick = (e) => {
+  e.preventDefault()
+}
+
+fillID()

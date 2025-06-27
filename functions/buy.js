2f97e42c3bf7812239a09917b6b2aa3a1cee6526
Azizbek <thedroxmc@gmail.com>
2025-06-27 21:01:02 +0500

    Success alert for purchase page

--- functions/buy.js
+++ functions/buy.js
@@ -1,7 +1,9 @@
 let accIDInput = document.querySelector(".accIDInput");
 let buyBtn = document.querySelector(".buyBtn");
-let amount = document.querySelector(".amount");
+let amount = document.querySelector(".amountInput");
 let userTelegram = localStorage.getItem("telegram");
+let xBtn = document.querySelector(".x-btn");
+let successAlert = document.querySelector(".success-alert");
 
 async function getUsers() {
   await axios
@@ -24,12 +26,30 @@ async function fillID() {
 
 buyBtn.onclick = async (e) => {
   e.preventDefault();
-  await axios.post(`https://67c8964c0acf98d07087272b.mockapi.io/purchaseReq`, {
-    createdAt: new Date().toISOString(),
-    accID: "1",
-    status: "waiting",
-    amount: amount.value,
-  });
+  await axios
+    .post(`https://67c8964c0acf98d07087272b.mockapi.io/purchaseReq`, {
+      createdAt: new Date().toISOString(),
+      accID: await axios
+        .get(
+          `https://67c8964c0acf98d07087272b.mockapi.io/users?telegram=${userTelegram}`
+        )
+        .then((response) => {
+          return response.data[0].accID;
+        })
+        .catch((err) => alert("Error Fetching AccID" + " " + err)),
+      status: "waiting",
+      amount: amount.value,
+    })
+    .then((response) => {
+      console.log(response);
+      successAlert.style.display = "flex";
+      amount.value = " "
+    })
+    .catch((err) => alert("Error While Posting a Purchase" + " " + err));
+};
+
+xBtn.onclick = () => {
+  successAlert.style.display = "none";
 };
 
 fillID();

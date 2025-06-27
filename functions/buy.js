bd1d7509de4b5e946a4625b0d8a5b9e420ce0543
Azizbek <thedroxmc@gmail.com>
2025-06-27 12:00:08 +0500

    Buy page design

---
+++ functions/buy.js
@@ -0,0 +1,9 @@
+// let accIDInput = document.querySelector(".accIDInput");
+
+// async function getUsers() {
+//   let userList = await axios.get("https://67c8964c0acf98d07087272b.mockapi.io/users");
+
+//   return(userList)
+// };
+
+// getUsers()
